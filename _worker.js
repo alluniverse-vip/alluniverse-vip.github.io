19d191ae0354922d0e2aa853e55d298512d8d9c5
alluniverse-vip <alluniverse-vip@users.noreply.github.com>
2024-03-27 10:56:08 +0000

    deploy: 87537ebf95fcedac70469ca277c33a0582874c46

---
+++ _worker.js
@@ -0,0 +1,16 @@
+import { Hono } from "hono";
+import { serveStatic } from 'hono/cloudflare-pages'
+
+const app = new Hono();
+
+app.get('/hello', (c) => {
+  return c.json({
+    message: `Hello`,
+    author: '墨渐生微',
+    date: new Date().toLocaleString()
+  })
+})
+
+// app.get("*", (ctx) => ctx.env.ASSETS.fetch(ctx.req.raw));
+app.get("*", (ctx, next) => serveStatic()(ctx, next));
+export default app;
