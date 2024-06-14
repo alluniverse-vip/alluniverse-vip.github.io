2b38ac22f7d06daba3a7bccbad31c353f3f40ef6
moon.pan <moon.pan@eclicktech.com.cn>
2024-06-14 11:17:53 +0800

    支持cloudflarespeed

--- _worker.js
+++ _worker.js
@@ -3,6 +3,23 @@ import { serveStatic } from 'hono/cloudflare-pages'
 
 const app = new Hono();
 
+// CloudflareSpeedTest
+app.get('/test1', async (c) => {
+  const fetchUrl = 'https://testfileorg.netwet.net/500MB-CZIPtestfile.org.zip'
+  const response = await fetch(fetchUrl)
+  return response;
+})
+
+addEventListener("fetch", event => {
+  let url = new URL(event.request.url);
+  if (url.pathname == "/test" && url.search == "") {
+    url.href="https://testfileorg.netwet.net/500MB-CZIPtestfile.org.zip"
+    let request = new Request(url, event.request);
+    event.respondWith(fetch(request));
+  }
+})
+
+// author
 app.get('/hello', (c) => {
   return c.json({
     message: `Hello`,
